d48272ce0d02e682e3198e1b713e3023a10b14e5
songopa <songopa@prefix.co.tz>
2024-09-30 10:56:02 +0300

    learing 03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(6);
+        Fraction f3 = new Fraction(6,7);
+
+        Console.WriteLine($"{f1.GetFractionString()}, {f2.GetFractionString()}, {f3.GetFractionString()}");
+
+        f1.SetTop(3);
+        f1.SetBottom(4);
+        Console.WriteLine($"{f1.GetTop()}, {f1.GetBottom()}");
+        Console.WriteLine($"{f1.GetFractionString()}");
+        Console.WriteLine($"{f1.GetDecimalValue()}");
+
     }
 }
