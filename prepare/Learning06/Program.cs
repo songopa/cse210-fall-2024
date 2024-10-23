18b2a8ac237833c097d69a4eabef9c9f0ef471c2
songopa <songopa@prefix.co.tz>
2024-10-23 19:29:02 +0300

    Learning 06

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        Square s1 = new Square(3.5);
+        s1.SetColor("Red");
+        Console.WriteLine($"\nColor: {s1.GetColor()} \nArea: {s1.GetArea()}");
+
+        Square s2 = new Square(7);
+        s2.SetColor("Brown");
+
+        Rectangle r1 = new Rectangle(3, 13);
+        r1.SetColor("Blue");
+
+        Circle c1 = new Circle(10);
+        c1.SetColor("White");
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(s2);
+        shapes.Add(r1);
+        shapes.Add(c1);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"\nColor: {shape.GetColor()} \nArea: {shape.GetArea()}");
+        }
+
+
     }
 }
